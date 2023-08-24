f61de751a512bfe1b0e58351ef8df3c210f1477c
Nezerwa <eligrand2000@gmail.com>
2023-08-24 19:56:39 +0200

    Add challenge README file

--- addall/addall.js
+++ addall/addall.js
@@ -1,22 +1,11 @@
 function addAll(numbers) {
-  const findPermutations = (arr = []) => {
-    let res = [];
-    const helper = (arr2) => {
-      if (arr2.length == arr.length) return res.push(arr2);
-      for (let e of arr) if (!arr2.includes(e)) helper([...arr2, e]);
-    };
-    helper([]);
-    return res;
-  };
-
-  let permutation = findPermutations(numbers);
-
-  return permutation.map((element) => {
-    return element.reduce((accumulator, currentValue) => {
-      accumulator = accumulator + currentValue;
-      accumulator = accumulator + currentValue;
-      return accumulator;
-    });
-  }, 0);
+  if (numbers.length <= 1) {
+    return 0;
+  }
+  numbers.sort((a, b) => a - b);
+  const sumOfFirstNumber = numbers[0] + numbers[1];
+  const newArr = [sumOfFirstNumber, ...numbers.slice(2)];
+  return sumOfFirstNumber + addAll(newArr);
 }
-console.log(addAll([1, 2, 3, 4]));
+
+console.log(addAll([1, 2, 3, 4, 5]));
