695fd55528dd0fb98cba61b993a2a6c8801f2d69
Nezerwa <eligrand2000@gmail.com>
2023-09-14 22:25:59 +0200

    Add challenge solution

--- ReverseInplace.js
+++ ReverseInplace.js
@@ -1,3 +1,5 @@
+// Reverse in  place
+// If you have a string like "I am the good boy". How can you generate "I ma eht doog yob"? Please note that the words are in place but reversed.
 const reverseInPlace = (str) => {
   let newStr = str.split(" ").map((elt) => {
    return elt.split("").reverse().join(''); 
