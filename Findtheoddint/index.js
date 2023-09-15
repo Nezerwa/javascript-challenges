b16db609c7eed46df53d66f6d9cd023b68c25d23
Nezerwa <eligrand2000@gmail.com>
2023-09-15 20:06:51 +0200

    Add final solution

--- Findtheoddint/index.js
+++ Findtheoddint/index.js
@@ -5,6 +5,7 @@ function findOdd(A) {
   });
   const newObj = Object.entries(count)
   const filtered = Object.fromEntries( newObj.filter(([key, value])=> value % 2 !== 0))
+  return parseInt(Object.keys(filtered));
 }
 
 console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
