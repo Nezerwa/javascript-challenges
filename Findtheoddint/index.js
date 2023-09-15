beb5183597ddb18e464bbfb9388059bc54574751
Nezerwa <eligrand2000@gmail.com>
2023-09-15 20:02:28 +0200

    filter based on the accurency that is odd

--- Findtheoddint/index.js
+++ Findtheoddint/index.js
@@ -1,6 +1,10 @@
 function findOdd(A) {
-  let newArray = A
-  console.log(newArray);
+  const count = {};
+  A.forEach((element) => {
+    count[element] ? (count[element] += 1) : (count[element] = 1);
+  });
+  const newObj = Object.entries(count)
+  const filtered = Object.fromEntries( newObj.filter(([key, value])=> value % 2 !== 0))
 }
 
 console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
