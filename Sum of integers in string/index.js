95cd3b2170a1dc17f840d92a64b80214fa1dec89
Nezerwa <eligrand2000@gmail.com>
2023-09-16 12:28:57 +0200

    Debug the error

--- Sum of integers in string/index.js
+++ Sum of integers in string/index.js
@@ -1,6 +1,15 @@
 function sumOfIntegersInString(s) {
-    //get all numbers inside a string
-  let newArray = s.match(/\d/g).join('');
+  //get all numbers inside a string
+  //   let match = s.match(/\d/g);
+  let newArray = [];
+  let newArrayOfString = s.split("");
+  newArrayOfString.forEach((element) => {
+    if (element.match(/\d/) !== null) {
+      newArray.push(element);
+    } else {
+      return element;
+    }
+  });
   console.log(newArray);
 }
 console.log(
