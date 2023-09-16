ea7e46dbb105608a3407edffa341f0a30d969361
Nezerwa <eligrand2000@gmail.com>
2023-09-16 11:42:25 +0200

    Add sum  of integer challenge README

--- Findtheoddint/index.js
+++ Findtheoddint/index.js
@@ -7,5 +7,4 @@ function findOdd(A) {
   const filtered = Object.fromEntries( newObj.filter(([key, value])=> value % 2 !== 0))
   return parseInt(Object.keys(filtered));
 }
-
 console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
