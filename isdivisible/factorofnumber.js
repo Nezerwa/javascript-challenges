48f35c71dad15ea81d8b2eb34510b0dbe5ce5004
Nezerwa <eligrand2000@gmail.com>
2023-09-20 12:27:09 +0200

    Add new challenge

--- isdivisible/factorofnumber.js
+++ isdivisible/factorofnumber.js
@@ -1,4 +1,10 @@
-function factors(x) {
-console.log(x);
-}
-console.log(factors(54));
+// function factors(x) {
+//   if (x < 0 || typeof x !== 'number' || Number.isInteger(x) === false) return -1;
+//   let newArr = [];
+//   for (let i = 1; i < x; i++) {
+//     if (x % i === 0) newArr.push(i);
+//   }
+//   console.log(x);
+//   return newArr.sort((a, b) => b - a);
+// }
+// console.log(factors(45.5));
