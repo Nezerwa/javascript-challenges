b2a47d7030197a939a9b3728268fcdaac893774a
Nezerwa <eligrand2000@gmail.com>
2023-10-04 19:15:27 +0200

    Add new challenge

---
+++ replacestring.js
@@ -0,0 +1,9 @@
+// function fakeBin(x) {
+//  return x.split("").map((number) => {
+//   return  number >= "5" ?  "1" :  "0";
+
+//   }).join('');
+// }
+
+const fakeBin = (x) => "00001111"[n]
+console.log(fakeBin("45385593107843568"));
